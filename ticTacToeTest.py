19253837d8a96cf400702872d029d6bf4a962511
Gisli Gudmundsson <gisli@iteggs.com>
2018-10-13 12:43:03 +0000

    Finished fixing the winning checker

--- ticTacToeTest.py
+++ ticTacToeTest.py
@@ -1,7 +1,5 @@
-import re
-
 board = []
-board_size = 3
+board_size = 4
 
 def create_board(board_size):
     t = 1
@@ -27,46 +25,70 @@ print(board)
 modify_board("1","x")
 modify_board("5","x")
 modify_board("9","x")
+modify_board("13","x")
 
 print(board)
 print("")
 
-# Virkar fínt
+# Check each row if there is a win
 def check_row_winner(board, player):
-    for x in range(board_size):
-        winner = True
-        for y in range(board_size):
-            if board[x][y] != player:                
-                winner = False
+    winner = False
+    for x in range(len(board)):
+        counter_row = 0
+        for y in range(len(board)):
+            if board[x][y] == player:
+                counter_row += 1
+                if counter_row == len(board):
+                    winner = True
                 continue
-
-        if winner == True:
-            return winner
     return winner
 
+# Checks columns down if there is a win
 def check_col_winner(board, player):
-    for x in range(board_size):
-        winner = True
-        for y in range(board_size):
-            if board[y][x] != player:                
-                winner = False
+    winner = False
+    for x in range(len(board)):
+        counter_col = 0
+        for y in range(len(board)):
+            if board[y][x] == player:
+                counter_col += 1
+                if counter_col == len(board):
+                    winner = True
                 continue
-
-        if winner == True:
-            return winner
     return winner
 
-def check_diag_winner(board, player):
-    for x in range(board_size):
-        winner = True
-        for y in range(board_size):
-            if board[x][x] != player:                
-                winner = False
-                continue
-        return winner
+# Checks diagonal rows from left to right
+def check_dia_winner_lr(board, player):
+    winner = False
+    counter_dia = 0
 
-print(check_col_winner(board,"X"))
-print(check_row_winner(board,"X"))
-print(check_diag_winner(board,"X"))
+    for x in range(len(board)):
+        if board[x][x] == player:
+            counter_dia += 1
 
-print("")
+        if counter_dia == len(board):
+            winner = True
+    return winner
+
+# Checks diagonal rows from left to right
+def check_dia_winner_rl(board, player):
+    winner = False
+    counter_dia = 0
+    reverse_count = 0
+    for x in reversed(range(len(board))):
+        
+        if board[x][reverse_count] == player:
+            counter_dia += 1
+        reverse_count += 1
+
+        if counter_dia == len(board):
+            winner = True
+    return winner
+
+# Evaluates the board and then figures out who is the winner
+def check_winner_board(board, player):
+    if check_row_winner(board,player) == True or check_col_winner(board,player) == True or check_dia_winner_lr(board,player) == True or check_dia_winner_rl(board,player) == True:
+        return True
+    else:
+        return False
+
+print(check_winner_board(board,"X"))
